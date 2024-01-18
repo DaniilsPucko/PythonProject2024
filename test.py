f40be89bc5c44d682c0db65d1fed10066f4d8a85
Даниил Пучко <mondex123@gmail.com>
2024-01-18 21:12:22 +0200

    test2

--- test.py
+++ test.py
@@ -24,10 +24,10 @@ def read_txt_file(english):
         print(f"File not found: {english}")
         return None
 
-def translate_to_latvian(words):
+def translate_to_language(words, target_language):
     translated_dict = {}
     driver = webdriver.Chrome()  
-    driver.get("https://www.bing.com/translator?to=lv&setlang=be")
+    driver.get(f"https://www.bing.com/translator?from=en&to={target_language}&setlang=be")
     
     try:
         element = WebDriverWait(driver, 10).until(
@@ -44,7 +44,7 @@ def translate_to_latvian(words):
                 find = driver.find_element(By.ID, 'tta_output_ta')
                 translation = find.get_attribute("value")
 
-                print(f"{word} (English) -> {translation} (Latvian)")
+                print(f"{word} (English) -> {translation} ({target_language.capitalize()})")
                 translated_dict[word] = translation
 
             except Exception as e:
@@ -59,18 +59,22 @@ file_path = 'english.txt'
 words_to_translate = read_txt_file(file_path)
 
 if words_to_translate is not None:
-    translated_words = translate_to_latvian(words_to_translate)
+    translated_words_latvian = translate_to_language(words_to_translate, 'lv')
+    translated_words_french = translate_to_language(words_to_translate, 'fr')
     
-    if translated_words:
+    if translated_words_latvian and translated_words_french:
         excel_file_path = 'translated_words.xlsx'
         workbook = openpyxl.Workbook()
         sheet = workbook.active
         sheet.cell(row=1, column=1, value='English Word')
         sheet.cell(row=1, column=2, value='Latvian Translation')
+        sheet.cell(row=1, column=3, value='French Translation')
 
-        for i, (word, translation) in enumerate(translated_words.items(), start=2):
+        for i, (word, translation_latvian) in enumerate(translated_words_latvian.items(), start=2):
+            translation_french = translated_words_french.get(word, '')
             sheet.cell(row=i, column=1, value=word)
-            sheet.cell(row=i, column=2, value=translation)
+            sheet.cell(row=i, column=2, value=translation_latvian)
+            sheet.cell(row=i, column=3, value=translation_french)
 
         workbook.save(excel_file_path)
         print(f"Translated words saved to {excel_file_path}")
