1629de05f1349b8d8b5c6c66ab91d7cce9512ebb
Даниил Пучко <mondex123@gmail.com>
2024-01-18 20:55:17 +0200

    testing python file and results in xlsx

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 import selenium
 import time
 from selenium import webdriver
@@ -25,7 +25,7 @@ def read_txt_file(english):
         return None
 
 def translate_to_latvian(words):
-    translated_list=[]
+    translated_dict = {}
     driver = webdriver.Chrome()  
     driver.get("https://www.bing.com/translator?to=lv&setlang=be")
     
@@ -45,7 +45,7 @@ def translate_to_latvian(words):
                 translation = find.get_attribute("value")
 
                 print(f"{word} (English) -> {translation} (Latvian)")
-                translated_list.append(translation)
+                translated_dict[word] = translation
 
             except Exception as e:
                 print(f"Translation failed for {word}: {e}")
@@ -53,7 +53,7 @@ def translate_to_latvian(words):
             time.sleep(2)
 
     driver.quit()
-    return translated_list
+    return translated_dict
 
 file_path = 'english.txt'
 words_to_translate = read_txt_file(file_path)
@@ -65,8 +65,12 @@ if words_to_translate is not None:
         excel_file_path = 'translated_words.xlsx'
         workbook = openpyxl.Workbook()
         sheet = workbook.active
-        for i, translation in enumerate(translated_words, start=1):
-            sheet.cell(row=i, column=1, value=translation)
+        sheet.cell(row=1, column=1, value='English Word')
+        sheet.cell(row=1, column=2, value='Latvian Translation')
+
+        for i, (word, translation) in enumerate(translated_words.items(), start=2):
+            sheet.cell(row=i, column=1, value=word)
+            sheet.cell(row=i, column=2, value=translation)
 
         workbook.save(excel_file_path)
         print(f"Translated words saved to {excel_file_path}")
